a411c60fd8b3dc1e32f63d03e298c52519d4d952
Reza Mousavi <MRez321@gmail.com>
2026-06-10 17:43:57 -0700

    new changes

--- server/src/controllers/download.controller.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from 'express';
-import { streamZip } from '../services/download.service';
-
-export const downloadController = {
-  downloadZip(_req: Request, res: Response): void {
-    streamZip(res);
-  },
-};
