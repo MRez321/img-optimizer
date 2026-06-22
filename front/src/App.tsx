cdaa09b5582eb5a172e801887e13b6a5a9f88346
Reza Mousavi <MRez321@gmail.com>
2026-06-21 21:55:42 -0700

    added footer

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,37 +3,26 @@ import { Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { Header } from './components/Header';
 import { AuthModal } from './components/AuthModal';
+import { Footer } from './components/Footer';
 import { OptimizerPage } from './pages/OptimizerPage';
 import { AccountPage } from './pages/AccountPage';
 
-import DateAndTime from "./components/DateAndTime/DateAndTime";
-// import './App.css'
-
 function App() {
-    const [authModalOpen, setAuthModalOpen] = useState(false);
+  const [authModalOpen, setAuthModalOpen] = useState(false);
 
-    return (
-        <AuthProvider>
-            <Header onAuthClick={() => setAuthModalOpen(true)} />
-            <main className="app-main">
-                <Routes>
-                    <Route path="/" element={<OptimizerPage />} />
-                    <Route path="/account" element={<AccountPage />} />
-                </Routes>
-            </main>
-            <AuthModal open={authModalOpen} onClose={() => setAuthModalOpen(false)} />
-        </AuthProvider>
-    );
-}
-
-function Rcomp() {
-    return (
-        <>
-            <h4>From Extra Comp</h4>
-            <DateAndTime />
-        </>
-    )
+  return (
+    <AuthProvider>
+      <Header onAuthClick={() => setAuthModalOpen(true)} />
+      <main className="app-main">
+        <Routes>
+          <Route path="/" element={<OptimizerPage />} />
+          <Route path="/account" element={<AccountPage />} />
+        </Routes>
+      </main>
+      <Footer />
+      <AuthModal open={authModalOpen} onClose={() => setAuthModalOpen(false)} />
+    </AuthProvider>
+  );
 }
 
 export default App;
-export { Rcomp }
